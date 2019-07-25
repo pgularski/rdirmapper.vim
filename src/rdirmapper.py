824c21596827cbba1df0dc083d01c4929246272c
Piotr Gularski <piotr.gularski@gmail.com>
2019-07-25 16:58:56 +0200

    Find the directory containing the .rdirmapper file

--- src/rdirmapper.py
+++ src/rdirmapper.py
@@ -1,7 +1,41 @@
 import vim
+import os
+import configparser
+
+RDIRMAPPER_FILENAME = '.rdirmapper'
+ROOT_DIR = '/'
+
+
+def upper_directory(path):
+    return os.path.split(os.path.abspath(path))[0]
+
+
+def get_rdirmapper_dir():
+    current_file = vim.current.buffer.name
+    current_dir = os.path.abspath(os.path.dirname(current_file))
+    while current_dir != ROOT_DIR:
+        if RDIRMAPPER_FILENAME in os.listdir(current_dir):
+            return current_dir
+        current_dir = upper_directory(current_dir)
+    return ''
+
+
 
 def say_it_works():
-    print("It works!")
+    rdrirmapper_dir = get_rdirmapper_dir()
+    if not rdrirmapper_dir:
+        print('{} file not found!'.format(RDIRMAPPER_FILENAME))
+        return
+
+    print(rdrirmapper_dir)
+
+    config = configparser.ConfigParser()
+    config.read(os.path.join(rdrirmapper_dir, RDIRMAPPER_FILENAME))
+    print(config.sections())
+
+
+    #  print("It works!")
+    #  print(os.getcwd())
     # vim.current.line
     #  print(vim.current.buffer.name)
     #  print(vim.vars.keys())
